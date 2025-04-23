011beaffc2826794a949f59ecfdf1dac88d0fe53
aleksSiema <aleks@siema.com.ua>
2025-04-23 18:30:56 +0300

    feat: base files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from '../shared/ui/auth-layout/auth-layout.component';
+import { LoginComponent } from '../pages/login/login.component';
+import { SignupComponent } from '../pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      // { path: '', redirectTo: 'profile/me', pathMatch: 'full' },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+
+    ],
+    
+  },
+];
