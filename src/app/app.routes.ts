fa1b9c68903c9824b9799cea72bff9bf6b23d3fe
aleksSiema <aleks@siema.com.ua>
2025-04-23 18:47:02 +0300

    feat: styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from '../shared/ui/auth-layout/auth-layout.component';
 import { LoginComponent } from '../pages/login/login.component';
 import { SignupComponent } from '../pages/signup/signup.component';
+import { ForgotPasswordComponent } from '../pages/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
-    {
+  {
     path: '',
     component: AuthLayoutComponent,
     children: [
@@ -12,8 +13,7 @@ export const routes: Routes = [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
-
+      { path: 'forgot-password', component: ForgotPasswordComponent },
     ],
-    
   },
 ];
