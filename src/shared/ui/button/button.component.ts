4ff1941fe44aadde7b94787e0adbe643384a0529
aleksSiema <aleks@siema.com.ua>
2025-05-06 20:39:07 +0300

    feat: button

--- src/shared/ui/button/button.component.ts
+++ src/shared/ui/button/button.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-button',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input({ transform: booleanAttribute }) fullWidth: boolean = false;
+  @Input({ transform: booleanAttribute }) disabled: boolean = false;
+
+  toggle() {
+    console.log(this.fullWidth);
+  }
+}
