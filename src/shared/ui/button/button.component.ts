e4674f2730b76154f36a418c7ff623d13a018907
aleksSiema <aleks@siema.com.ua>
2025-05-07 18:37:56 +0300

    feat: button

--- src/shared/ui/button/button.component.ts
+++ src/shared/ui/button/button.component.ts
@@ -12,6 +12,8 @@ type ButtonVariant = 'filled' | 'outlined';
 export class ButtonComponent {
   @Input({ transform: booleanAttribute }) fullWidth: boolean = false;
   @Input({ transform: booleanAttribute }) disabled: boolean = false;
+  @Input({ transform: booleanAttribute }) loading: boolean = false;
+
   @Input() onClick!: () => void;
   @Input() type: ButtonVariant = 'filled';
 }
