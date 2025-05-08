136f96d6a3a7e04e14a5f90235ab5b43ba4535e8
aleksSiema <aleks@siema.com.ua>
2025-05-08 20:17:42 +0300

    feat: mainlayout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,25 @@
 import { Routes } from '@angular/router';
-import { AuthLayoutComponent } from '../shared/ui/auth-layout/auth-layout.component';
+import { AuthLayoutComponent } from '../widgets/auth-layout/auth-layout.component';
 import { LoginComponent } from '../pages/login/login.component';
 import { SignupComponent } from '../pages/signup/signup.component';
 import { ForgotPasswordComponent } from '../pages/forgot-password/forgot-password.component';
+import { MainLayoutComponent } from '../widgets/main-layout/main-layout.component';
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: AuthLayoutComponent,
     children: [
-      // { path: '', redirectTo: 'profile/me', pathMatch: 'full' },
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
       { path: 'forgot-password', component: ForgotPasswordComponent },
     ],
+  },
+  {
+    path: 'dashboard',
+    component: MainLayoutComponent,
+    children: [{ path: '', component: DashboardComponent }],
   },
 ];
