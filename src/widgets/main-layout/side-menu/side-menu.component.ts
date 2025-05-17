1cca29bbf91eaa90ead7c4f3821d48ba02d682a6
aleksSiema <aleks@siema.com.ua>
2025-05-17 18:06:15 +0300

    feat: side menu

--- src/widgets/main-layout/side-menu/side-menu.component.ts
+++ src/widgets/main-layout/side-menu/side-menu.component.ts
@@ -1,11 +1,25 @@
 import { LogoComponent } from '@/shared/ui/logo/logo.component';
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'side-menu',
-  imports: [RouterLink, LogoComponent],
+  imports: [RouterLink, LogoComponent, NgClass],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  isCollapsed = false;
+
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  links = [
+    { title: 'Dashboard', path: 'dashboard' },
+    { title: 'Agile Board', path: 'agile-board' },
+    { title: 'Calendar', path: 'calendar' },
+    { title: 'Backlog', path: 'backlog' },
+  ];
+}
