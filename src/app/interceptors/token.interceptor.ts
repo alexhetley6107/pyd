a1a755edf06447627c16c74e5b35584aff4005a8
alexhetley6107 <hetleyofficial@gmail.com>
2025-07-30 19:27:55 +0300

    [PYD] Add 401 handling

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { AuthService } from '@/shared/services/auth.service';
 
 @Injectable()
@@ -10,13 +16,19 @@ export class TokenInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.auth.getToken();
 
-    if (token) {
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${token}`),
-      });
-      return next.handle(cloned);
-    }
+    const authReq = token
+      ? req.clone({
+          headers: req.headers.set('Authorization', `Bearer ${token}`),
+        })
+      : req;
 
-    return next.handle(req);
+    return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.auth.logout();
+        }
+        return throwError(() => error);
+      })
+    );
   }
 }
