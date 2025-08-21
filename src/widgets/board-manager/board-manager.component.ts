89977e22113656c62c7786ef4a6e337ca4c46302
alexhetley6107 <hetleyofficial@gmail.com>
2025-08-21 18:41:24 +0300

    [PYD] Update select

--- src/widgets/board-manager/board-manager.component.ts
+++ src/widgets/board-manager/board-manager.component.ts
@@ -1,13 +1,19 @@
 import { ButtonComponent } from '@/shared/ui/button/button.component';
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { BoardModalComponent } from '@/features/board-modal/board-modal.component';
 import { BoardDeleteModalComponent } from '@/features/board-delete-modal/board-delete-modal.component';
 import { TaskModalComponent } from '@/features/task-modal/task-modal.component';
 import { BoardService } from '@/shared/services/board.service';
 import { PopoverComponent } from '@/shared/ui/popover/popover.component';
-import { SelectComponent } from '@/shared/ui/select/select.component';
 import { ActionOption, SelectOption } from '@/shared/types/ui';
 import { SkeletonComponent } from '@/shared/ui/skeleton/skeleton.component';
+import { SelectorComponent } from '@/shared/ui/selector/selector.component';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'board-manager',
@@ -16,9 +22,10 @@ import { SkeletonComponent } from '@/shared/ui/skeleton/skeleton.component';
     BoardModalComponent,
     PopoverComponent,
     BoardDeleteModalComponent,
-    SelectComponent,
     SkeletonComponent,
     TaskModalComponent,
+    SelectorComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './board-manager.component.html',
   styleUrl: './board-manager.component.scss',
@@ -30,6 +37,30 @@ export class BoardManagerComponent {
   isEditModal = false;
   isDeleteModal = false;
   isTaskModal = false;
+
+  form!: FormGroup<{
+    boardId: FormControl<string>;
+  }>;
+
+  constructor(private fb: NonNullableFormBuilder) {
+    effect(() => {
+      const boardId = this.boardService.openedBoard()?.id;
+      if (!boardId) return;
+      this.form.patchValue({ boardId });
+    });
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      boardId: this.fb.control(''),
+    });
+
+    this.form.valueChanges.subscribe((value) => {
+      if (value?.boardId) {
+        this.boardService.openBoard(value.boardId);
+      }
+    });
+  }
 
   get isBoardSelect(): boolean {
     return this.boardService.boards.length > 0;
@@ -44,12 +75,6 @@ export class BoardManagerComponent {
       label: board.name,
       value: board.id,
     }));
-  }
-
-  get selectedBoardOption(): SelectOption | null {
-    return (
-      this.boardSelectOptions.find((o) => o.value === this.boardService?.openedBoard()?.id) ?? null
-    );
   }
 
   selectedBoardActions: ActionOption[] = [
@@ -74,9 +99,5 @@ export class BoardManagerComponent {
   }
   toggleTaskModal() {
     this.isTaskModal = !this.isTaskModal;
-  }
-
-  onOpenBoard(boardId: string | null) {
-    this.boardService.openBoard(boardId);
   }
 }
