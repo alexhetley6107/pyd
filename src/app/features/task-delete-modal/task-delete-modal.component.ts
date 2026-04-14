73b42974f7f28a4b14ae18207bed99fe54f41657
alexhetley6107 <hetleyofficial@gmail.com>
2026-04-14 17:49:49 +0300

    [PYD] Refactor

--- src/app/features/task-delete-modal/task-delete-modal.component.ts
+++ src/app/features/task-delete-modal/task-delete-modal.component.ts
@@ -4,6 +4,8 @@ import { ToastService } from '@/shared/services/toast.service';
 import { Component, computed, inject, input, output, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ConfirmModalComponent } from '@/shared/ui/confirm-modal/confirm-modal.component';
+import { Nullable } from '@/shared/types';
+import { Task } from '@/entities/task/model';
 
 @Component({
   selector: 'task-delete-modal',
@@ -16,27 +18,32 @@ export class TaskDeleteModalComponent {
   route = inject(ActivatedRoute);
   router = inject(Router);
 
-  boardId = computed(() => this.route.snapshot.queryParamMap.get('boardId') ?? null);
-  taskId = computed(() => this.route.snapshot.paramMap.get('taskId') ?? null);
+  task = input.required<Nullable<Task>>();
 
   open = input<boolean>(false);
   closeModal = output();
+
+  isBacklogTask = computed<boolean>(() => this.router.url.includes(ERoute.BACKLOG));
+
+  isLoading = signal(false);
 
   onCloseModal() {
     this.closeModal.emit();
   }
 
-  isLoading = signal(false);
-
   onDeleteTask() {
     this.isLoading.set(true);
 
-    const taskId = this.taskId() ?? '';
+    const taskId = this.task()?.id ?? '';
 
     this.taskService.delete(taskId).subscribe({
       next: () => {
         this.toast.success(`Task successfully deleted.`);
-        this.router.navigate([ERoute.BOARDS, this.boardId() ?? '']);
+        const backUrl = this.isBacklogTask()
+          ? ERoute.BACKLOG
+          : `/${ERoute.BOARDS}/${this.task()?.boardId ?? ''}`;
+
+        this.router.navigate([backUrl]);
       },
       error: (err: any) => {
         this.toast.error(err.error.message);
